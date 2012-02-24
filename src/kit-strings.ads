e07755cad9b38f91e43442f3744c5059c55098b3
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2012-02-24 17:48:43 +0100

    initial length set to 0

--- src/kit-strings.ads
+++ src/kit-strings.ads
@@ -3,7 +3,7 @@ package Kit.Strings is
    type String_Type (Max_Length : Natural) is
       record
          Text     : String (1 .. Max_Length);
-         Length   : Natural;
+         Length   : Natural := 0;
       end record;
 
 end Kit.Strings;
