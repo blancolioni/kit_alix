6ebdcb332fa95521a5bbf7e1e3e1bdb4f37119bd
Fraser Wilson <blancolioni@gmail.com>
2012-02-27 22:09:41 +0100

    Removed keyword 'compound'.   Added compound key parsing.

--- src/kit-parser-tokens.ads
+++ src/kit-parser-tokens.ads
@@ -4,7 +4,7 @@ private package Kit.Parser.Tokens is
       (Tok_None, Tok_End_Of_File, Tok_Bad_Character,
        Tok_Identifier, Tok_Integer_Constant, Tok_String_Constant,
 
-       Tok_Compound, Tok_End, Tok_Is, Tok_Key, Tok_Package,
+       Tok_End, Tok_Is, Tok_Key, Tok_Package,
        Tok_Record, Tok_Type, Tok_Unique,
 
        Tok_Colon, Tok_Semi, Tok_Left_Paren, Tok_Right_Paren, Tok_Comma,
