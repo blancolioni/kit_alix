8383ffda7c78bc39fad8ac6f0a11f33b9d78860a
Fraser Wilson <blancolioni@gmail.com>
2012-03-09 12:37:32 +0100

    added reportRecord binding for Leander

--- config/Kit.hs
+++ config/Kit.hs
@@ -1,5 +1,13 @@
 module Kit where
 
+class DatabaseRecord a where
+  get :: a -> String -> IO String
+  set :: a -> String -> String -> IO ()
+
+class Search a where
+  hasElement :: a -> Bool
+  next :: a -> IO a
+
 primFirstRootRecord :: Int -> Int
 primFirstRootRecord = primitive #firstRootRecord
 
@@ -11,3 +19,15 @@ primRootRecordGetTopRecord = primitive #rootRecordGetTopRecord
 
 rootRecordGetTopRecord :: Int -> IO Int
 rootRecordGetTopRecord = return . primRootRecordGetTopRecord
+
+primNamedItemGetName :: Int -> String
+primNamedItemGetName = primitive #namedItemGetName
+
+namedItemGetName :: Int -> IO String
+namedItemGetName = return . primNamedItemGetName
+
+primReportRecord :: Int -> Int -> Int
+primReportRecord = primitive #reportRecord
+
+reportRecord :: Int -> Int -> IO Int
+reportRecord table index = return $ primReportRecord table index
