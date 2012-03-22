fc6dd897a951d51db93d0f93bf1b37f8e1c96a09
Fraser Wilson <blancolioni@gmail.com>
2012-03-22 15:14:04 +0100

    Added close procedure for freeing database records

--- src/kit.adb
+++ src/kit.adb
@@ -1,4 +1,18 @@
+with Ada.Unchecked_Deallocation;
+
 package body Kit is
+
+   -----------
+   -- Close --
+   -----------
+
+   procedure Close (Rec : in out Database_Record) is
+      procedure Free is
+        new Ada.Unchecked_Deallocation (Root_Database_Record'Class,
+                                        Database_Record);
+   begin
+      Free (Rec);
+   end Close;
 
    ---------
    -- Get --
