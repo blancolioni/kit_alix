2dc102ec9fb3ce012b3c5ad92245a61aeca0dca4
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2012-11-14 15:07:02 +0100

    added Leander support; cleaned server file generate a bit

--- src/kit-generate-server.adb
+++ src/kit-generate-server.adb
@@ -66,19 +66,42 @@ package body Kit.Generate.Server is
       Target_Directory : String)
    is
       Standard_Sub : Substitution_Maps.Map;
-      Key : Substitution_Key;
+      Key          : Substitution_Key;
+
+      procedure Copy (Package_Name : String);
+
+      ----------
+      -- Copy --
+      ----------
+
+      procedure Copy (Package_Name : String) is
+         Source_Spec : constant String :=
+                         Make_File_Name (Package_Name, "ads");
+         Source_Body : constant String :=
+                         Make_File_Name (Package_Name, "adb");
+         Target_Spec : constant String :=
+                         Make_File_Name (Database_Name & "." & Package_Name,
+                                         "ads");
+         Target_Body : constant String :=
+                         Make_File_Name (Database_Name & "." & Package_Name,
+                                         "adb");
+      begin
+         Copy_File (Kit.Paths.Config_Path & "/" & Source_Spec,
+                    Target_Directory & "/" & Target_Spec,
+                    Standard_Sub);
+         Copy_File (Kit.Paths.Config_Path & "/" & Source_Body,
+                    Target_Directory & "/" & Target_Body,
+                    Standard_Sub);
+      end Copy;
+
    begin
       Ada.Strings.Fixed.Move ("database", Key);
       Standard_Sub.Insert (Key, Database_Name);
 
-      Copy_File (Kit.Paths.Config_Path & "/tables.ads",
-                 Target_Directory & "/"
-                 & Make_File_Name (Database_Name & ".Tables", "ads"),
-                 Standard_Sub);
-      Copy_File (Kit.Paths.Config_Path & "/tables.adb",
-                 Target_Directory & "/"
-                 & Make_File_Name (Database_Name & ".Tables", "adb"),
-                 Standard_Sub);
+      Copy ("Tables");
+      Copy ("Tables.SK_Tables");
+      Copy ("SK_Bindings");
+
    end Copy_Server_Packages;
 
    --------------------
