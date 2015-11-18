433c2c999ae4ef921614acef644eee99864fbc7e
Fraser Wilson <blancolioni@gmail.com>
2015-11-18 14:01:40 +0100

    Case 190: generating Table_Names package

---
+++ src/generator/kit-generate-table_name_map.ads
@@ -0,0 +1,9 @@
+with Aquarius.Drys.Declarations;
+
+package Kit.Generate.Table_Name_Map is
+
+   function Generate_Package
+     (Db : in out Kit.Schema.Databases.Database_Type)
+      return Aquarius.Drys.Declarations.Package_Type;
+
+end Kit.Generate.Table_Name_Map;
