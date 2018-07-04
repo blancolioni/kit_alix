c4f0591810aaa277880601578bffd506f20ea700
Fraser Wilson <blancolioni@gmail.com>
2018-07-04 14:46:40 +0200

    Replaced XML dependency with Partoe

--- src/kit-import-xml_db.ads
+++ src/kit-import-xml_db.ads
@@ -1,11 +1,11 @@
-with XML;
+with Partoe;
 with Kit.Schema.Databases;
 
 package Kit.Import.XML_DB is
 
    function XML_DB_Reader
      (Db : Kit.Schema.Databases.Database_Type)
-      return XML.XML_Document'Class;
+      return Partoe.Partoe_Document'Class;
 
 --     type XML_DB_Reader is
 --       new XML.XML_Document with private;
