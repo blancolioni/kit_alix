787d957e91b8cd1eabe032917543ee6412f3722e
Fraser Wilson <blancolioni@gmail.com>
2019-02-06 15:23:12 +0100

    New package Kit.Version

---
+++ src/kit-version.ads
@@ -0,0 +1,13 @@
+package Kit.Version is
+
+   Version_Major    : constant := 0;
+   Version_Minor    : constant := 3;
+   Version_Release  : constant := 0;
+   Build_Number     : constant := 0;
+
+   Version_String   : constant String := "0.3.0.0";
+   Build_String     : constant String := "0";
+
+   Component_Name   : constant String := "Kit";
+
+end Kit.Version;
