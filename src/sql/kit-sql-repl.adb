c1c663c860b0432f0600dd7b25cffaf64df0963b
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-04-04 12:34:16 +0200

    Removed baked in option from REPL

--- src/sql/kit-sql-repl.adb
+++ src/sql/kit-sql-repl.adb
@@ -13,51 +13,35 @@ package body Kit.SQL.Repl is
 
    procedure Start_Repl is
    begin
-      if True then
+      loop
+         Ada.Text_IO.Put ("SQL> ");
+         Ada.Text_IO.Flush;
          declare
-            Query : Kit.SQL.Queries.Query_Element;
+            Line : constant String := Ada.Text_IO.Get_Line;
          begin
-            Query.Create ("select * from kit_record;");
-            if not Query.Is_Empty then
-               declare
-                  Data : Kit.SQL.Data_Tables.Data_Table;
-               begin
-                  Query.Execute (Data);
-                  Kit.SQL.Reports.Report (Data);
-               end;
+            if Line = "exit" then
+               exit;
             end if;
-         end;
-      else
-         loop
-            Ada.Text_IO.Put ("SQL> ");
-            Ada.Text_IO.Flush;
-            declare
-               Line : constant String := Ada.Text_IO.Get_Line;
-            begin
-               if Line = "exit" then
-                  exit;
-               end if;
 
-               declare
-                  Query : Kit.SQL.Queries.Query_Element;
-               begin
-                  Query.Create (Line);
-                  if not Query.Is_Empty then
-                     declare
-                        Data : Kit.SQL.Data_Tables.Data_Table;
-                     begin
-                        Query.Execute (Data);
-                        Kit.SQL.Reports.Report (Data);
-                     end;
-                  end if;
-               end;
-            exception
-               when E : others =>
-                  Ada.Text_IO.Put_Line
-                    (Ada.Exceptions.Exception_Message (E));
+            declare
+               Query : Kit.SQL.Queries.Query_Element;
+            begin
+               Query.Create (Line);
+               if not Query.Is_Empty then
+                  declare
+                     Data : Kit.SQL.Data_Tables.Data_Table;
+                  begin
+                     Query.Execute (Data);
+                     Kit.SQL.Reports.Report (Data);
+                  end;
+               end if;
             end;
-         end loop;
-      end if;
+         exception
+            when E : others =>
+               Ada.Text_IO.Put_Line
+                 (Ada.Exceptions.Exception_Message (E));
+         end;
+      end loop;
    exception
       when E : others =>
          Ada.Text_IO.Put_Line
